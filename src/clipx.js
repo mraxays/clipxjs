230a10f9325791ec2f297c5d4ba66f735b65a015
MrAkshayAS <akshau72p@gmail.com>
2025-04-01 23:20:38 +0530

    Add SVG icons to toast notifications for success and error messages

--- src/clipx.js
+++ src/clipx.js
@@ -147,7 +147,9 @@
     if (!disableToast && !this.disable.toast) {
       var toast = document.createElement("div");
       toast.className = toastClass;
-      toast.textContent = customMessage;
+      // Insert the success SVG icon along with the custom message
+      var successSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgba(0,210,65,1)" fill="currentColor"><path d="M4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12ZM12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM17.4571 9.45711L16.0429 8.04289L11 13.0858L8.20711 10.2929L6.79289 11.7071L11 15.9142L17.4571 9.45711Z"></path></svg>`;
+      toast.innerHTML = successSVG + customMessage;
       toast.setAttribute("role", "alert");
       toast.setAttribute("aria-live", "assertive");
       toast.style.cssText = this.customStyles.toast || ""; // Apply custom styles if any
@@ -186,7 +188,9 @@
     console.error("Failed to cut/copy text: ", err);
     var toast = document.createElement("div");
     toast.className = "clipx-toast error";
-    toast.textContent = "Failed to copy!";
+    // Insert the error SVG icon along with the error message
+    var errorSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="rgba(231,0,0,1)" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path></svg>`;
+    toast.innerHTML = errorSVG + "Failed to copy!";
     toast.setAttribute("role", "alert");
     toast.setAttribute("aria-live", "assertive");
     toast.style.cssText = this.customStyles.toast || "";
@@ -227,6 +231,9 @@
     transition: opacity 0.5s ease;
     max-width: 90%; /* Ensure it fits well on smaller screens */
     width: auto; /* Ensure the width adjusts based on content */
+    display: flex;
+    align-items: center;
+    gap: 5px;
 }
 
 .clipx-toast.top {
